69fd7184a7921481e784d4b5007991f182f2aa6f
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-04-11 14:31:19 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 async function Login  (userName, password){
-
     var users = await fetch("./users.json").then((response) => {
         return response.json();
     });
     users=users.user;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-    
       var user = users.find(
         (user) => user.name === userName && user.password === password
       );
@@ -22,7 +20,6 @@ document.getElementById("submitForm").addEventListener("submit", async (e) => {
   e.preventDefault();
   let userName=document.querySelector("#email").value;
   let password = document.querySelector("#password").value;
-
   try {
     console.log("call");
     let user = await Login(userName, password);
@@ -30,7 +27,7 @@ document.getElementById("submitForm").addEventListener("submit", async (e) => {
         window.location.href="./time.html";
     }
     console.log("User logged in:", user);
-  } catch (error) {
+  }catch (error) {
     console.error("Login error:", error.message);
     alert("Login error:", error.message);
   }
